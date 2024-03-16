b4fbf89b65884a00c70be39eadd74116932f0d1c
Dallin Sly <krave83@gmail.com>
2024-03-15 22:11:36 -0400

    side scrolling iss ues

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,36 +10,36 @@ function Footer() {
       <div className="footer__text">© 2020 Supersite, Powered by News API</div>
       <div className="footer__buttons-right">
         <div className="footer__middle-right">
-        <Link to="/" className="footer__button">
-          Home
-        </Link>{" "}
-        {/* Using Link for internal routing */}
-        <a
-          href="https://tripleten.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer__button"
-        >
-          Triple Ten
-        </a>
-      </div>
-      <div className="footer__far-right">
-        <a
-          href="https://github.com/Dsly1998"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer__icon-button"
-        >
-          <img src={github} alt="github icon" />
-        </a>
-        <a
-          href="https://www.facebook.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="footer__icon-button"
-        >
-          <img src={facebook} alt="facebook icon" />
-        </a>
+          <Link to="/" className="footer__button">
+            Home
+          </Link>{" "}
+          {/* Using Link for internal routing */}
+          <a
+            href="https://tripleten.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__button"
+          >
+            Triple Ten
+          </a>
+        </div>
+        <div className="footer__far-right">
+          <a
+            href="https://github.com/Dsly1998"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__icon-button"
+          >
+            <img src={github} alt="github icon" />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer__icon-button footer__icon-button--facebook"
+          >
+            <img src={facebook} alt="facebook icon" />
+          </a>
         </div>
       </div>
     </footer>
