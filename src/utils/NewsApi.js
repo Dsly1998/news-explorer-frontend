e214bef38a2e4f3f39ab8d14e3839c4a863a8f3d
Dallin Sly <krave83@gmail.com>
2024-04-12 13:54:49 -0400

    corect

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,15 +1,19 @@
 // newsApi.js
 
-const API_KEY = '7d5aa8655b694d14aef74cac887fb786';
-const BASE_URL = 'https://newsapi.org/v2';
+const API_KEY = "7d5aa8655b694d14aef74cac887fb786";
+const BASE_URL = "https://nomoreparties.co/news/v2";
 
 const fetchNews = async (keyword) => {
   const fromDate = new Date();
   fromDate.setDate(fromDate.getDate() - 7);
   const toDate = new Date();
-  const formatDate = (date) => date.toISOString().split('T')[0];
+  const formatDate = (date) => date.toISOString().split("T")[0];
 
-  const url = `${BASE_URL}/everything?q=${encodeURIComponent(keyword)}&from=${formatDate(fromDate)}&to=${formatDate(toDate)}&sortBy=publishedAt&pageSize=100&apiKey=${API_KEY}`;
+  const url = `${BASE_URL}/everything?q=${encodeURIComponent(
+    keyword
+  )}&from=${formatDate(fromDate)}&to=${formatDate(
+    toDate
+  )}&sortBy=publishedAt&pageSize=100&apiKey=${API_KEY}`;
 
   try {
     const response = await fetch(url);
@@ -18,9 +22,12 @@ const fetchNews = async (keyword) => {
     }
     const data = await response.json();
     // Include the search keyword in each article object
-    return data.articles.map(article => ({ ...article, searchKeyword: keyword }));
+    return data.articles.map((article) => ({
+      ...article,
+      searchKeyword: keyword,
+    }));
   } catch (error) {
-    console.error('Fetch News Error:', error);
+    console.error("Fetch News Error:", error);
     throw error;
   }
 };
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://articlelist.ignorelist.com"
+    ? "https://api.articlelist.ignorelist.com"
     : "http://localhost:";
 
 // Fetch articles by a user
