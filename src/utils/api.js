e62a0a0924c7efd4649c4971cd730894569d565d
Dallin Sly <krave83@gmail.com>
2024-04-12 15:35:34 -0400

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://api.articlelist.ignorelist.com"
-    : "http://localhost:";
+    : "http://localhost:3001";
 
 // Fetch articles by a user
 export const getArticlesByUser = async (token) => {
