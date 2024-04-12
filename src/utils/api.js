b495ca9b868d2dcbb90302552de03135ca11304b
Dallin Sly <krave83@gmail.com>
2024-04-12 00:26:17 -0400

    cors

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://articlelist.ignorelist.com"
-    : "http://localhost:3001";
+    : "http://localhost:";
 
 // Fetch articles by a user
 export const getArticlesByUser = async (token) => {
